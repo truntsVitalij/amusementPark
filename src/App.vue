a784edfa9e2309f526f32262ad24c6ddd3847687
Vitalij Trunts <trunts.vitalij1110@gmail.com>
2021-10-22 09:13:36 +0300

    priceSection

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,48 @@
   <div id="app">
     <home-section></home-section>
     <about-section></about-section>
+    <amusement-catalog-section></amusement-catalog-section>
     <price-section></price-section>
+    <feed-back-section></feed-back-section>
     <contacts-section></contacts-section>
+    <footer>
+      <div class="columns is-align-items-center">
+        <div class="column">Designed by Trunts Vitalij</div>
+        <div class="column">+375259513199</div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
 import AboutSection from "./sections/AboutSection.vue";
+import AmusementCatalogSection from "./sections/AmusementCatalogSection.vue";
 import ContactsSection from "./sections/ContactsSection.vue";
+import FeedBackSection from "./sections/FeedBackSection.vue";
 import HomeSection from "./sections/HomeSection.vue";
 import PriceSection from "./sections/PriceSection.vue";
 export default {
-  components: { HomeSection, AboutSection, ContactsSection, PriceSection },
+  components: {
+    HomeSection,
+    AboutSection,
+    ContactsSection,
+    PriceSection,
+    AmusementCatalogSection,
+    FeedBackSection,
+  },
   name: "App",
+  data() {
+    return {
+      isOpenModal: false,
+    };
+  },
 };
 </script>
 <style lang="scss">
 @import "styles/style.scss";
+footer {
+  font-size: 11px;
+  height: 80px;
+  background: black;
+}
 </style>;
